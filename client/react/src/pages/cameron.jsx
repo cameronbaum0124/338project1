854874bb6066eafd8de1cc4c75abfc4271c3a0e6
cameronbaum0124 <cameronbaum0124@gmail.com>
2026-03-27 12:36:41 -0400

    Second commit test for individual branches

--- client/react/src/pages/cameron.jsx
+++ client/react/src/pages/cameron.jsx
@@ -5,6 +5,7 @@ function Cameron() {
             <button onClick={() => window.location.href="/"}>Back to Home</button>
             <br></br>
             <button onClick={() => console.log("Hello! This is being used to test merging among different branches!")}>Merge Test</button>
+            <h2>This is a second test for merging and individual branches.</h2>
         </div>
     );
 }
