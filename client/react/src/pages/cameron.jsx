ab6436a54d20dff62074fdd23a87b8e28598189a
cameronbaum0124 <cameronbaum0124@gmail.com>
2026-03-27 12:24:40 -0400

    This is a test for merging changes done in our individual branches

--- client/react/src/pages/cameron.jsx
+++ client/react/src/pages/cameron.jsx
@@ -3,6 +3,8 @@ function Cameron() {
         <div>
             <h1>Welcome to Cameron's Page</h1>
             <button onClick={() => window.location.href="/"}>Back to Home</button>
+            <br></br>
+            <button onClick={() => console.log("Hello! This is being used to test merging among different branches!")}>Merge Test</button>
         </div>
     );
 }
